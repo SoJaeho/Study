8c73a6ff466be87da802175636c0265b7b240a81
SoJaeho <145527348+SoJaeho@users.noreply.github.com>
2023-11-14 21:03:09 +0900

    Delete cardatabase directory

--- cardatabase/src/main/java/com/packt/cardatabase/domain/AccountCredentials.java
+++
@@ -1,9 +0,0 @@
-package com.packt.cardatabase.domain;
-import lombok.Getter; 
-import lombok.Setter; 
-@Getter 
-@Setter 
-public class AccountCredentials { 
-	private String username; 
-	private String password; 
-}
